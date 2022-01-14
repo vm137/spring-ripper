9c49c1dbb329a6603d54f729613b5f3f8c15d98c
vic.m@mail.ru <Action137>
2022-01-14 10:35:24 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Spring Ripper
+
+Евгений Борисов — Spring-потрошитель, часть 1
+https://youtu.be/BmBr5diz8WA
---
+++ src/main/java/quoters/TerminatorQuoter.java
@@ -0,0 +1,31 @@
+package quoters;
+
+import javax.annotation.PostConstruct;
+
+@Profiling
+public class TerminatorQuoter implements Quoter {
+    @InjectRandomInt(min = 2, max = 7)
+    private int repeat;
+    private String message;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Phase 2");
+        System.out.println("repeat: " + repeat);
+    }
+
+    public TerminatorQuoter() {
+        System.out.println("Phase 1");
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public void sayQuote() {
+        for (int i = 0; i < repeat; i++) {
+            System.out.println("Message: " + message);
+        }
+    }
+}
